b372b4e1020c9257841af6e8038ba0ba118b9a51
Joshdb-18 <joshuadami17@gmail.com>
2023-06-02 09:21:32 +0100

    Star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const request = require('request');
 const movieId = process.argv[2];
 
 const options = {
-  url: `https://swapi-api.alx-tools.com/api/`,
+  url: `https://swapi-api.alx-tools.com/api/films/${movieId}/`,
   method: 'GET',
   headers: {
     'User-Agent': 'request',
